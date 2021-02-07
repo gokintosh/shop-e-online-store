a6ca540219a89e3ccf59abb60a29f12dd0270668
GOKUL KANNAMBALLY BALACHNADRAN <52125327+gokintosh@users.noreply.github.com>
2021-02-07 11:03:04 +0100

    initialize spring backend

---
+++ backend/src/main/java/com/gokul/shopebackend/ShopEBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gokul.shopebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopEBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopEBackendApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/gokul/shopebackend/ShopEBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gokul.shopebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopEBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

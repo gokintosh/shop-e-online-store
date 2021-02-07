209ab29dadc0e6f369fc78e52a7288709ac57d7d
GOKUL KANNAMBALLY BALACHNADRAN <52125327+gokintosh@users.noreply.github.com>
2021-02-07 21:41:54 +0100

    complete list all products

--- backend/src/main/java/com/gokul/shopebackend/controller/ProductController.java
+++ backend/src/main/java/com/gokul/shopebackend/controller/ProductController.java
@@ -1,10 +1,23 @@
 package com.gokul.shopebackend.controller;
 
 
+import com.gokul.shopebackend.model.Product;
+import com.gokul.shopebackend.service.ProductServices;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/product")
 public class ProductController {
+
+    @Autowired
+    ProductServices productServices;
+
+    @RequestMapping("getAll")
+    public List<Product> getAllProducts(){
+        return productServices.getAllProducts();
+    }
 }
--- backend/src/main/java/com/gokul/shopebackend/repository/ProductReository.java
+++ backend/src/main/java/com/gokul/shopebackend/repository/ProductReository.java
@@ -2,6 +2,9 @@ package com.gokul.shopebackend.repository;
 
 import com.gokul.shopebackend.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 
 public interface ProductReository extends JpaRepository<Product,Long> {
+
 }

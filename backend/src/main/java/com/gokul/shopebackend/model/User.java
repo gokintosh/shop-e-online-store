e3e7bb18dbb839cb54eb02639e3262b83493dfec
GOKUL KANNAMBALLY BALACHNADRAN <52125327+gokintosh@users.noreply.github.com>
2021-02-10 01:04:15 +0100

    add jwt login

--- backend/src/main/java/com/gokul/shopebackend/model/User.java
+++ backend/src/main/java/com/gokul/shopebackend/model/User.java
@@ -2,8 +2,10 @@ package com.gokul.shopebackend.model;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name="users")
 public class User {
     @Id
     long id;
--- backend/src/main/java/com/gokul/shopebackend/service/UserServices.java
+++
@@ -1,4 +0,0 @@
-package com.gokul.shopebackend.service;
-
-public class UserServices {
-}
